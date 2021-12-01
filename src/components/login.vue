a437099f3bc4f011cbc3d2b990a4ab55932a1424
Aniketos <aniketos@AniketosdeMacBook-Pro.local>
2021-12-01 12:25:25 +0800

    first

---
+++ src/components/login.vue
@@ -0,0 +1,12 @@
+<template>
+ <h3>主页</h3>
+  <router-view></router-view>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Home'
+})
+</script>
---
+++ src/utils/request.ts
@@ -0,0 +1,5 @@
+/**
+ * axios封装
+ */
+import axios from 'axios'
+import config from './../config/config'
